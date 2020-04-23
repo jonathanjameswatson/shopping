e98e7a438782547d67632421ae656c3db1d43c7c
Jonathan Watson <jonathanw12@outlook.com>
2020-04-23 12:00:56 +0100

    Added basic layout

---
+++ components/Item.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="media">
+    <div class="media-left">
+      <div class="field">
+        <b-checkbox v-model="checked" size="is-large" />
+      </div>
+    </div>
+    <div class="media-content is-size-4">
+      {{ name }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    editMode: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    section: {
+      type: String,
+      required: true
+    },
+    checked: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
